31efb689e82cf818c8305f91f35af22bc994b259
root <yluo3421@gmail.com>
2022-05-31 15:50:04 -0400

    shuffle, double number of cards, max-width, but not max-height

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,20 +3,53 @@ import Card from './Card'
 
 
 function Game() {
+
   const [tiles, setTiles]=useState([])
+  
+
   useEffect(()=>
   {
     fetch('http://localhost:3001/mahjong')
     .then(response => response.json())
-    .then(data => setTiles(data));
+    .then(data => {
+      setTiles(data)
+      
+    });
   }, [])
+
+  
+
+  // this method create random array of cards to be used
+  function shuffleCards(array) {
+    const length = array.length;
+    for (let i = length; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * i);
+      const currentIndex = i - 1;
+      const temp = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temp;
+    }
+    return array;
+  }
+
+  let randomCards = shuffleCards(tiles.concat(tiles))
+  //console.log(randomCards);
+
+  function handleTileClick() {
+
+  }
   
   return (
-    <div>
+    <div className="gamePage">
       <h1>Play Card Matching Game!</h1>
       <div className='container'>
-          {tiles.map((tile)=>{
-            return <Card key={tile.id} tile={tile}/>})
+          {randomCards.map((tile, index)=>{
+            return <Card 
+              key={index} 
+              tile={tile}
+              index={index}
+              onClick={handleTileClick}
+            />})
           }
       </div>
     </div>
