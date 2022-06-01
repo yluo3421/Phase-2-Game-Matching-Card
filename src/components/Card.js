cc33104cbc13495030419690c229c13c891989ef
Tashi Dolma <tadolma97@gmail.com>
2022-06-01 11:34:40 -0400

    Flipped Tile

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Card({tile}){
+function Card({tile, onClick}){
+    const {id}=tile
+    const [isFlipped, setIsFlipped]=useState(false)
+    
+    function handleClick(){
+        setIsFlipped(!isFlipped)
+        onClick(id)
+    }
+
+    // function handleTileClick(){
+    //     console.log(index)
+    // }
+    const backTile="https://raw.githubusercontent.com/FluffyStuff/riichi-mahjong-tiles/9e2eba21ca69d6cd5cdd9a309d5754ac9517c481/Regular/Front.svg"
     return (
-    <div className="tile">
-        <img alt="tile" src= {tile.image}/>
+    <div className="tile" >
+        {isFlipped?<img alt="tile" src= {tile.image} onClick={handleClick}/>:
+        <img alt="tile" src= {backTile} onClick={handleClick}/>}
     </div>
     )
 }
