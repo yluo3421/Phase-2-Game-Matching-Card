fe081853e6f4e8a150543d2f51dfcb79cf5adaa2
root <yluo3421@gmail.com>
2022-06-02 10:42:34 -0400

    POST for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 
 function Login() {
   const history=useHistory();
-  function handleSubmit(e){  
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {  
     e.preventDefault()
-    history.push("/Game")}
+    fetch("http://localhost:3002/leader", {
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name:name,
+        moves:0
+      }),
+    })
+    history.push("/Game")
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h1>Login</h1>
       <div>
-        <input type="text" name="username" placeholder="Username" />
+        <input 
+        type="text" 
+        name="username" 
+        placeholder="Username"
+        value={name}
+        onChange={(e) => setName(e.target.value)} 
+        />
       </div>
       <input type="submit" value="Submit" />
     </form>
