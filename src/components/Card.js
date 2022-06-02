eca861e361729e13ad5da4b91f398eb15a26680f
Tashi Dolma <tadolma97@gmail.com>
2022-06-02 09:18:15 -0400

    flipping state and useEffect

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,17 @@
-import React, {useState} from "react";
+import React from "react";
 
-function Card({tile, onClick}){
-    const {id}=tile
-    const [isFlipped, setIsFlipped]=useState(false)
-    
+function Card({tile, onClick, flipped}){
+
     function handleClick(){
-        setIsFlipped(!isFlipped)
-        onClick(id)
+        onClick(tile)
     }
-
-    // function handleTileClick(){
-    //     console.log(index)
-    // }
     const backTile="https://raw.githubusercontent.com/FluffyStuff/riichi-mahjong-tiles/9e2eba21ca69d6cd5cdd9a309d5754ac9517c481/Regular/Front.svg"
     return (
     <div className="tile" >
-        {isFlipped?<img alt="tile" src= {tile.image} onClick={handleClick}/>:
-        <img alt="tile" src= {backTile} onClick={handleClick}/>}
+        <div className={flipped ? "flipped" : ""}>
+            <img className="front" alt="tile front" src= {tile.image}/> 
+            <img className="back" alt="tile back" src= {backTile} onClick={handleClick}/>
+        </div>
     </div>
     )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,90 +1,69 @@
 import React, {useState, useEffect} from "react";
 import Card from './Card'
-
+function shuffleCards(data) {
+  const shuffledCards=[...data, ...data]
+    .sort(()=>Math.random()- 0.5)
+    .map((card)=>({...card, id: Math.random()}))
+  return shuffledCards
+}
 
 function Game() {
 
   const [tiles, setTiles]=useState([])
-  const [flippedTile, setFlippedTile]=useState([])
-  // const [isFlipped, setIsFlipped]=useState(false)
-  const emptyState = [];
-  
+  const [isMatched, setIsMatched] = useState([])
+  const [clickOne, setClickOne]=useState(null)
+  const [clickTwo, setClickTwo]=useState(null)
+
 
   useEffect(()=>
   {
     fetch('http://localhost:3001/mahjong')
     .then(response => response.json())
     .then(data => {
-      setTiles(shuffleCards(data.concat(data)))
+      setTiles(shuffleCards(data))
       
     });
   }, [])
 
+  function handleClick(tile){
+    clickOne?setClickTwo(tile):setClickOne(tile)
+  }
 
-  // function handleFlippedCounter(){
-  //     setFlippedCounter((flippedCounter)=>flippedCounter+1)
-  // }
-  // console.log(flippedCounter)
-
-  // this method create random array of cards to be used
-
-  function handleClick(id){
-
-    setFlippedTile((flippedTile)=>[...flippedTile, id])
-    // console.log(flippedTile)
-
-    if (flippedTile.length===2){
-      if (flippedTile[0]===flippedTile[1]){
-        console.log('yyaayy')
-
+  useEffect(()=>{
+    if (clickOne && clickTwo){
+      if (clickOne.name === clickTwo.name){
+        resetClicks()
+        setIsMatched((prev)=>[...prev,clickOne.name])
+      }else {
+        setTimeout(()=>resetClicks(),1000)
       }
-      
-      setFlippedTile([id])
-      console.log(flippedTile + "on changeing to empty")
-      
     }
-    
-  }
-  
+  }, [clickOne, clickTwo])
 
-
-
-  
-
-  function shuffleCards(array) {
-    const length = array.length;
-    for (let i = length; i > 0; i--) {
-      const randomIndex = Math.floor(Math.random() * i);
-      const currentIndex = i - 1;
-      const temp = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temp;
-    }
-    return array;
+  function resetClicks(){
+    setClickOne(null)
+    setClickTwo(null)
   }
 
-  // let randomCards = shuffleCards(tiles.concat(tiles))
-  //console.log(randomCards);
+  useEffect(()=>{
+    checkComplete()}, [isMatched])
 
-  // function handleTileClick() {
-  //     setIsFlipped(!isFlipped)
-  //     handleFlippedCounter()
-  // }
+  function checkComplete(){
+    if (isMatched.length===18){
+      console.log('win')
+    }
+  }
 
-
-  
   return (
     <div className="gamePage">
       <h1>Play Card Matching Game!</h1>
       <div className='container'>
-          {tiles.map((tile, index)=>{
+          {tiles.map((tile)=>{
             return <Card 
-              key={index} 
+              key={tile.id} 
               tile={tile}
-              index={index}
               onClick={handleClick}
-              // handleFlippedCounter={handleFlippedCounter}
-              // isFlipped={isFlipped}
+              flipped={tile===clickOne||tile===clickTwo||isMatched.includes(tile.name)}
             />})
           }
       </div>
