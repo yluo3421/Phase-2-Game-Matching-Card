3224bfd62deaad4b51b81aea2045868a2d8b5bf9
Tashi Dolma <tadolma97@gmail.com>
2022-06-03 09:43:20 -0400

    card flip transition, decrease game card to 15

---
+++ ExtraMahjong.js
@@ -0,0 +1,19 @@
+{
+    [
+        {
+            "id": 2,
+            "name": "Man2",
+            "image": "https://raw.githubusercontent.com/FluffyStuff/riichi-mahjong-tiles/9e2eba21ca69d6cd5cdd9a309d5754ac9517c481/Regular/Man2.svg"
+        }, 
+        {
+            "id": 3,
+            "name": "Sou6",
+            "image": "https://raw.githubusercontent.com/FluffyStuff/riichi-mahjong-tiles/9e2eba21ca69d6cd5cdd9a309d5754ac9517c481/Regular/Sou6.svg"
+        },
+        {
+            "id": 14,
+            "name": "Sou8",
+            "image": "https://raw.githubusercontent.com/FluffyStuff/riichi-mahjong-tiles/9e2eba21ca69d6cd5cdd9a309d5754ac9517c481/Regular/Sou8.svg"
+        }
+    ]
+}
